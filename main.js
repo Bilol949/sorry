b8b65939c9a10b0e2118c0b157c304e729b2f4f5
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-12-26 20:18:33 +0500

    desert

---
+++ main.js
@@ -0,0 +1,45 @@
+// let srt = "hello javascript";
+
+// let natija = srt.replace('javascript' , 'python');
+
+// console.log(natija);
+// let savol = prompt('ismingiz')
+
+// let soz = prompt('soz yozing')
+
+// let natija = savol + soz;
+// if(natija.includes(natija)){
+//     alert("bunday soz bor")
+// }else{
+//     alert("bunday soz yoq")
+// }
+
+// let soz = prompt("Ismingizni kiriting")
+// let soz2 = prompt("bronta soz kiriting")
+
+
+let soz1 = prompt('ismingizni kiriting');
+let soz  = prompt ("Bronta soz kiriting");
+
+
+
+
+
+ if(soz1.toUpperCase().includes(soz.toUpperCase())) {
+     alert(`${soz1} Ismining ichida ${soz} harifi mavjud`)
+ }else{
+    alert(`${soz1} Ismining ichida ${soz} harifi mavjud emas`)
+
+ }
+
+// let str = "hello javascript";  //python
+
+// let word = prompt('biror soz kiriting')
+
+
+// if(str.includes(word)){
+//     console.log('ha bor');
+    
+// }else{
+//     console.log('bunday soz yoq');
+// }
